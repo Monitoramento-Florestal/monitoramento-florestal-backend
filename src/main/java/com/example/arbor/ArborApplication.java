4a3626d8b6f64925b39cefe4f961a47d186b3ab3
Erikvgl <erikvinicius8847@gmail.com>
2026-04-01 16:11:57 -0300

    feat: setup inicial do backend

---
+++ src/main/java/com/example/arbor/ArborApplication.java
@@ -0,0 +1,13 @@
+package com.example.arbor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ArborApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ArborApplication.class, args);
+	}
+
+}
