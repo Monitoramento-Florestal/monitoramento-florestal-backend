8aca17da78905b7facebb3e76192ca4466967a44
MjwsantiagoN <134097753+MjwsantiagoN@users.noreply.github.com>
2026-05-17 05:45:55 -0300

    Rename EsqueciSenhaRequestDTO to SolicitarRecuperacaoDTO

--- src/main/java/com/example/arbor/dto/request/SolicitarRecuperacaoDTO.java
+++ src/main/java/com/example/arbor/dto/request/SolicitarRecuperacaoDTO.java
@@ -3,6 +3,6 @@ package com.example.arbor.dto.request;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
 
-public record EsqueciSenhaRequestDTO(
+public record SolicitarRecuperacaoDTO(
         @NotBlank @Email String email) {
 }
