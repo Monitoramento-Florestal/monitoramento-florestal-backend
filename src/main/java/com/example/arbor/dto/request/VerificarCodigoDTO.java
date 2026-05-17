f7301801594850771d026412a8baa9a316d3dd4a
MjwsantiagoN <134097753+MjwsantiagoN@users.noreply.github.com>
2026-05-17 14:51:58 -0300

    Add 'codigo' field to VerificarCodigoDTO

--- src/main/java/com/example/arbor/dto/request/VerificarCodigoDTO.java
+++ src/main/java/com/example/arbor/dto/request/VerificarCodigoDTO.java
@@ -5,7 +5,7 @@ import jakarta.validation.constraints.Size;
 import jakarta.validation.constraints.Email;
 
 public record VerificarCodigoDTO(
-        @NotBlank @Size(min = 6, max = 6) String codigo
+        @NotBlank @Size(min = 6, max = 6) String codigo,
         @NotBlank @Email String email
 ) {}
 
